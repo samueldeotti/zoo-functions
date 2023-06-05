ba2863c95d0f494b670891150cc54b33eab9091f
Samuel Henrique Bertolini Deotti <samuel_deotti@hotmail.com>
2023-06-05 14:20:51 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (ids, id2) => {
+  if (!ids) return [];
+  const dataEntries = Object.values(data)[0];
+  if (!id2) {
+    return [dataEntries.find((specie) => specie.id === ids)];
+  }
+
+  const arraySpecies = [];
+  dataEntries.forEach((specie) => {
+    if (specie.id === ids || specie.id === id2) arraySpecies.push(specie);
+  });
+
+  return arraySpecies;
 };
 
 module.exports = getSpeciesByIds;
