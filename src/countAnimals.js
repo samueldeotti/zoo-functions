195f6a3c8bd8c52c4e2bb35bbef422770dc540ed
Samuel Henrique Bertolini Deotti <samuel_deotti@hotmail.com>
2023-06-05 20:41:39 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  const dataEntries = Object.values(data)[0];
+  if (!animal) {
+    const obj = {};
+    const name = dataEntries.map((animalName) => animalName.name);
+    const quantity = dataEntries.map((size) => size.residents.length);
+    name.forEach((element, index) => {
+      obj[element] = quantity[index];
+    });
+    return obj;
+  }
+  const animalObj = Object.values(animal);
+  if (animalObj.length === 1) {
+    return dataEntries.find((animalName) => animalName.name === animalObj[0]).residents.length;
+  }
+  return dataEntries.find((animalName) => animalName.name === animalObj[0])
+    .residents.filter((animalSex) => animalSex.sex === animalObj[1]).length;
 };
 
 module.exports = countAnimals;
