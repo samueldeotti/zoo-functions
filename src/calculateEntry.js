572d20edd4255fd2ea4a656569de12c7aa6a6fd1
Samuel Henrique Bertolini Deotti <samuel_deotti@hotmail.com>
2023-06-06 15:49:59 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,24 @@
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui
+  const child = entrants.filter((people) => people.age < 18).length;
+  const adult = entrants.filter((people) => people.age >= 18 && people.age < 50).length;
+  const senior = entrants.filter((people) => people.age >= 50).length;
+  return { child, adult, senior };
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (!entrants) return 0;
+  const { prices } = data;
+  const quantity = Object.values(countEntrants(entrants));
+  const array = ['child', 'adult', 'senior'];
+  let value = 0;
+  quantity.forEach((element, index) => {
+    value += element * prices[array[index]];
+  });
+  return value;
+
+  // const quantity = Object.values(countEntrants(entrants)).reduce((acc, curr) => acc + curr, 0);
 };
 
 module.exports = { calculateEntry, countEntrants };
