02d1f10f9063319ba8d5e0be23d338bdc211d6e9
Samuel Henrique Bertolini Deotti <samuel_deotti@hotmail.com>
2023-06-14 16:46:57 -0300

    requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,14 +1,51 @@
 const data = require('../data/zoo_data');
 
-const getSchedule = (scheduleTarget) => { // alterar usando o ...rest
-  // const [x, y] = scheduleTarget, consigo colocar nome para a variavel que vier como parametro
-  const arraySpecies = data.species;
-  const animals = arraySpecies.some((specie) => specie.name === scheduleTarget);
-  const days = Object.keys(data.hours);
-  const isDay = days.some((day) => day === scheduleTarget);
-  if (animals || isDay) {
-    return arraySpecies.find((specie) => specie.name === scheduleTarget).availability;
-  }
+const msgMonday = () => ({
+  officeHour: 'CLOSED',
+  exhibition: 'The zoo will be closed!',
+});
+
+const openingData = (day) => {
+  const hours = Object.values(data.hours[day]);
+  if (day === 'Monday') return { Monday: msgMonday() };
+  return {
+    [day]: {
+      officeHour: `Open from ${hours[0]}am until ${hours[1]}pm`,
+      exhibition: data.species
+        .filter((specie) => specie.availability.indexOf(day) > -1)
+        .map((specie) => specie.name),
+    },
+  };
+};
+
+const allHours = (hours) => {
+  const days = Object.keys(hours);
+  return days.reduce((acc, day) => {
+    const hoursRunning = Object.values(data.hours[day]);
+    if (day === 'Monday') {
+      acc[day] = msgMonday();
+    } else {
+      acc[day] = {
+        officeHour: `Open from ${hoursRunning[0]}am until ${hoursRunning[1]}pm`,
+        exhibition: data.species
+          .filter((specie) => specie.availability.indexOf(day) > -1)
+          .map((specie) => specie.name),
+      };
+    }
+    return acc;
+  }, {});
+};
+
+const getSchedule = (scheduleTarget) => {
+  const { species, hours } = data;
+  const isDay = Object.keys(hours).find((day) => day === scheduleTarget);
+  if (isDay) return openingData(scheduleTarget);
+  if (!scheduleTarget) return allHours(hours);
+
+  const isAnimal = species.find((specie) =>
+    specie.name === scheduleTarget);
+  if (isAnimal) return isAnimal.availability;
+  return allHours(hours);
 };
 
 module.exports = getSchedule;
