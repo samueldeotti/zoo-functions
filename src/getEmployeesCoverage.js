d817680ac4bd9d0829def07683caa77e3afb9905
Samuel Henrique Bertolini Deotti <samuel_deotti@hotmail.com>
2023-06-15 14:22:05 -0300

    refatorando o 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,14 +1,17 @@
 const data = require('../data/zoo_data');
 
-const allEmployees = () => {
-  const { species, employees } = data;
+const animals = (species, curr) => {
   const allAnimals = species.map((specie) =>
     ({ id: specie.id, name: specie.name, location: specie.location }));
-  return employees.reduce((acc, curr) => {
-    const choosenAnimals = allAnimals
-      .filter((specieId) => curr.responsibleFor
-        .find((specie) => specie === specieId.id));
+  return allAnimals.filter((specieId) => curr.responsibleFor
+    .find((specie) => specie === specieId.id));
+};
 
+const allEmployees = () => {
+  const { species, employees } = data;
+
+  return employees.reduce((acc, curr) => {
+    const choosenAnimals = animals(species, curr);
     acc.push({ id: curr.id,
       fullName: `${curr.firstName} ${curr.lastName}`,
       species: choosenAnimals.map((specie) => specie.name),
