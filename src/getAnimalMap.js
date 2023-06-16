3781f0858fec57a65418814c90a46034f98fa32e
Samuel Henrique Bertolini Deotti <samuel_deotti@hotmail.com>
2023-06-16 09:09:39 -0300

    requisito 14

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,25 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+
+const locations = ['NE', 'NW', 'SE', 'SW'];
+
+const allLocation = () => locations.reduce((acc, curr) => {
+  acc[curr] = species.filter((specie) => specie.location === curr).map((specie) => specie.name);
+  return acc;
+}, {});
+
+const order = (a, b, sorted) => ((sorted) ? a.localeCompare(b) : 0);
+
+const namesAnimals = (allAnimals, sorted, sex) => locations.reduce((acc, curr) => {
+  acc[curr] = [...allAnimals[curr]]
+    .map((specieName) => ({ [specieName]: species.find((specie) => specie.name === specieName)
+      .residents.filter((resident) => !sex || resident.sex === sex)
+      .map((resident) => resident.name).sort((a, b) => order(a, b, sorted)) }));
+  return acc;
+}, {});
 
 const getAnimalMap = (options) => {
-  // seu código aqui
+  if (!options || !options.includeNames) return allLocation();
+  return namesAnimals(allLocation(), options.sorted, options.sex);
 };
 
 module.exports = getAnimalMap;
